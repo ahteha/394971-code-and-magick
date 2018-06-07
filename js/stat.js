c71354c1156ca39f841e348a7e25d02dab7f4ca0
Dmitri Prozorov <di.prozorov@gmail.com>
2018-06-07 16:19:56 +0300

    добавлено отображение времени над столбцами

--- js/stat.js
+++ js/stat.js
@@ -16,40 +16,42 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+var renderText = function (ctx, x, y, color, size, message) {
+  ctx.fillStyle = color;
+  ctx.font = size + 'px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText(message, x, y);
+};
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+var getMaxElement = function (array) {
+  var maxElement = array[0];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > maxElement) {
+      maxElement = array[i];
     }
   }
-
   return maxElement;
 };
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-  ctx.fillStyle = '#000';
-
-  ctx.font = FONT_SIZE + 'px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + TITLE_GAP, CLOUD_Y + TITLE_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + TITLE_GAP, CLOUD_Y + TITLE_GAP + FONT_SIZE);
+  renderText(ctx, CLOUD_X + TITLE_GAP, CLOUD_Y + TITLE_GAP, '#000', FONT_SIZE, 'Ура вы победили!');
+  renderText(ctx, CLOUD_X + TITLE_GAP, CLOUD_Y + TITLE_GAP + FONT_SIZE, '#000', FONT_SIZE, 'Список результатов:');
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], CLOUD_X + TITLE_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP);
+    renderText(ctx, CLOUD_X + TITLE_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP, '#000', FONT_SIZE, names[i]);
+
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() * 1 + ')';
     }
 
-    ctx.fillRect(CLOUD_X + TITLE_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - FONT_SIZE - GAP, BAR_WIDTH, -(MAX_BAR_HEIGHT * times[i]) / maxTime);
+    var barHeight = -(MAX_BAR_HEIGHT * times[i]) / maxTime;
+    ctx.fillRect(CLOUD_X + TITLE_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - FONT_SIZE - GAP, BAR_WIDTH, barHeight);
+    renderText(ctx, CLOUD_X + TITLE_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - (barHeight * -1) - BAR_GAP, '#000', FONT_SIZE, Math.round(times[i]));
   }
 };
